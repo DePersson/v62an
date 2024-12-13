cfcf77b11cf3cee42672a6dcfb110de7d1774b20
DePersson <depersson_81@telia.com>
2024-12-13 17:21:59 +0100

    Added Delete job function, installed toastify package

--- src/App.jsx
+++ src/App.jsx
@@ -11,6 +11,7 @@ import NotFoundPage from './pages/NotFoundPage';
 import JobPage, { jobLoader } from './pages/JobPage';
 import AddJobPage from './pages/addJobPage';
 
+// Add new job
 const App = () => {
   const addJob = async (newJob) => {
     const res = await fetch('/api/jobs', {
@@ -22,13 +23,21 @@ const App = () => {
     });
     return;
   };
+
+  // Delete job
+  const deleteJob = async (id) => {
+    const res = await fetch(`/api/jobs/${id}`, {
+      method: 'DELETE',
+    });
+    return;
+  };
   
   const router = createBrowserRouter(
     createRoutesFromElements(
       <Route path='/' element={<MainLayout />}>
         <Route index element={<HomePage />} />
         <Route path='/jobs' element={<JobsPage />} />
-        <Route path='/jobs/:id' element={<JobPage />} loader={jobLoader}/>
+        <Route path='/jobs/:id' element={<JobPage deleteJob={deleteJob}/>} loader={jobLoader}/>
         <Route path='/add-Job' element={<AddJobPage addJobSubmit={addJob} />} />
         <Route path='*' element={<NotFoundPage />} />
       </Route>
